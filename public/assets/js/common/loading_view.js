8c783d597e331ec39aad200c42415424d0ef0da4
Lisa @laptop <lcatdesigns@gmail.com>
2015-07-10 11:47:17 -0500

    Spinner working without spin.jquery

--- public/assets/js/common/loading_view.js
+++ public/assets/js/common/loading_view.js
@@ -2,13 +2,10 @@
 
 import $ from 'jquery';
 import Marionette from 'marionette';
-
-//import spin from '../vendor/spin';
-//require('../vendor/spin.jquery');
+import Spinner from 'vendor/spin';
 
 export default Marionette.ItemView.extend({
   template: 'loadingview',
-
   title: 'Loading Data',
   message: 'Please wait, data is loading.',
 
@@ -20,24 +17,26 @@ export default Marionette.ItemView.extend({
   },
 
   onShow: function() {
-//    var opts = {
-//      lines: 13, // The number of lines to draw
-//      length: 20, // The length of each line
-//      width: 10, // The line thickness
-//      radius: 30, // The radius of the inner circle
-//      corners: 1, // Corner roundness (0..1)
-//      rotate: 0, // The rotation offset
-//      direction: 1, // 1: clockwise, -1: counterclockwise
-//      color: '#000', // #rgb or #rrggbb
-//      speed: 1, // Rounds per second
-//      trail: 60, // Afterglow percentage
-//      shadow: false, // Whether to render a shadow
-//      hwaccel: false, // Whether to use hardware acceleration
-//      className: 'spinner', // The CSS class to assign to the spinner
-//      zIndex: 2e9, // The z-index (defaults to 2000000000)
-//      top: '30px', // Top position relative to parent in px
-//      left: 'auto' // Left position relative to parent in px
-//    };
-//    $('#spinner').spin(opts);
+    var opts = {
+      lines: 13, // The number of lines to draw
+      length: 20, // The length of each line
+      width: 10, // The line thickness
+      radius: 30, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '30px', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    };
+
+    var target = document.getElementById('spinner');
+    var spinner = new Spinner(opts).spin(target);
   }
 });

8cbe8ca96f3316776c27a3591506c946b32ea332
Lisa Catalano <@lcatdesigns (twitter)>
2017-06-22 13:44:12 -0500

    #1036 - Update project so ESLint works

--- autocomplete-dataprovider.js
+++ autocomplete-dataprovider.js
@@ -1,133 +1,309 @@
 /**
  * Provides data for autocomplete.
- * 
+ *
  * This element is seperated from autocomplete selector so it can deal with how data is retrieved by implementing filter method,  So data can be retireved by sync, async or cached irrespective of how autocomplete works
- * 
+ *
  * This element listens for  autocomplete-filtered-text event from the autocomplete-selector,
- * 
+ *
  */
 Polymer({
-      is: "autocomplete-dataprovider",
-      hostAttributes:{
-        
-      },
-      properties: {
-        /* 
-         * Autocomplete results. should be used to bind with autcomplete-selectors data.
-         * @type { Array => Object } Object Must have keys label and value
-         */
-        data : {
-            type : Array,
-            notify : true,
-        },
-        /*
-         * Retrieves data asynchronously
-         * 
-         * If this property is true, then filter method should return a promise which resolves to Array
-         * else it can return Array of Objects
-         */ 
-        remote : {
-            type : Boolean,
-            notify : true,
-            value : false
-        }
-      },
-      listeners : {
-          'autocomplete-filtered-text' :  '_getData'
-      },
-      _getData : function(event) {
-          var self = this;
-          var result = this.filter(event);
-          if(!this.remote) {
-              self.set('data', result)
-          } else {
-              result.then(function(response) {
-                self.set('data', response);
-            });
-          }
-      },
-      /*
-        * Allows customization of retrieving data.
-        * Example:
-        * document.querySelector('autocomplete-dataprovider').filter = function() { console.log('Implement custom logic')}
-        * 
-        * @returns {Promise<Array> | Array} depeneding on the remote property
-        * 
-        * 
-        */
-      filter : function(event) {
-          var text = event.detail.toUpperCase();
-          var states = [
-                {"label": "Alabama", "value": "AL"},
-                {"label": "Alaska", "value": "AK"},
-                {"label": "American Samoa", "value": "AS"},
-                {"label": "Arizona", "value": "AZ"},
-                {"label": "Arkansas", "value": "AR"},
-                {"label": "California", "value": "CA"},
-                {"label": "Colorado", "value": "CO"},
-                {"label": "Connecticut", "value": "CT"},
-                {"label": "Delaware", "value": "DE"},
-                {"label": "District Of Columbia", "value": "DC"},
-                {"label": "Federated States Of Micronesia", "value": "FM"},
-                {"label": "Florida", "value": "FL"},
-                {"label": "Georgia", "value": "GA"},
-                {"label": "Guam", "value": "GU"},
-                {"label": "Hawaii", "value": "HI"},
-                {"label": "Idaho", "value": "ID"},
-                {"label": "Illinois", "value": "IL"},
-                {"label": "Indiana", "value": "IN"},
-                {"label": "Iowa", "value": "IA"},
-                {"label": "Kansas", "value": "KS"},
-                {"label": "Kentucky", "value": "KY"},
-                {"label": "Louisiana", "value": "LA"},
-                {"label": "Maine", "value": "ME"},
-                {"label": "Marshall Islands", "value": "MH"},
-                {"label": "Maryland", "value": "MD"},
-                {"label": "Massachusetts", "value": "MA"},
-                {"label": "Michigan", "value": "MI"},
-                {"label": "Minnesota", "value": "MN"},
-                {"label": "Mississippi", "value": "MS"},
-                {"label": "Missouri", "value": "MO"},
-                {"label": "Montana", "value": "MT"},
-                {"label": "Nebraska", "value": "NE"},
-                {"label": "Nevada", "value": "NV"},
-                {"label": "New Hampshire", "value": "NH"},
-                {"label": "New Jersey", "value": "NJ"},
-                {"label": "New Mexico", "value": "NM"},
-                {"label": "New York", "value": "NY"},
-                {"label": "North Carolina", "value": "NC"},
-                {"label": "North Dakota", "value": "ND"},
-                {"label": "Northern Mariana Islands", "value": "MP"},
-                {"label": "Ohio", "value": "OH"},
-                {"label": "Oklahoma", "value": "OK"},
-                {"label": "Oregon", "value": "OR"},
-                {"label": "Palau", "value": "PW"},
-                {"label": "Pennsylvania", "value": "PA"},
-                {"label": "Puerto Rico", "value": "PR"},
-                {"label": "Rhode Island", "value": "RI"},
-                {"label": "South Carolina", "value": "SC"},
-                {"label": "South Dakota", "value": "SD"},
-                {"label": "Tennessee", "value": "TN"},
-                {"label": "Texas", "value": "TX"},
-                {"label": "Utah", "value": "UT"},
-                {"label": "Vermont", "value": "VT"},
-                {"label": "Virgin Islands", "value": "VI"},
-                {"label": "Virginia", "value": "VA"},
-                {"label": "Washington", "value": "WA"},
-                {"label": "West Virginia", "value": "WV"},
-                {"label": "Wisconsin", "value": "WI"},
-                {"label": "Wyoming", "value": "WY"}
-            ];
-            var results = [];
-            if (text.length === 0) {
-                results =  states.splice(0);
-            }
-            else {
-                results = states.filter(function(state) {
-                    return (~state.label.toUpperCase().indexOf(text)) || (~state.value.toUpperCase().indexOf(text));
-                })
-            }
+  is: 'autocomplete-dataprovider',
+  hostAttributes: {
 
-            return  results;
+  },
+  properties: {
+    /*
+     * Autocomplete results. should be used to bind with autcomplete-selectors data.
+     * @type { Array => Object } Object Must have keys label and value
+     */
+    data: {
+      type: Array,
+      notify: true
+    },
+    /*
+     * Retrieves data asynchronously
+     *
+     * If this property is true, then filter method should return a promise which resolves to Array
+     * else it can return Array of Objects
+     */
+    remote: {
+      type: Boolean,
+      notify: true,
+      value: false
+    }
+  },
+  listeners: {
+    'autocomplete-filtered-text': '_getData'
+  },
+  _getData: function(event) {
+    var self = this;
+    var result = this.filter(event);
+    if (!this.remote) {
+      self.set('data', result)
+    } else {
+      result.then(function(response) {
+        self.set('data', response);
+      });
+    }
+  },
+  /*
+   * Allows customization of retrieving data.
+   * Example:
+   * document.querySelector('autocomplete-dataprovider').filter = function() { console.log('Implement custom logic')}
+   *
+   * @returns {Promise<Array> | Array} depeneding on the remote property
+   *
+   *
+   */
+  filter: function(event) {
+    var text = event.detail.toUpperCase();
+    var states = [
+      {
+        'label': 'Alabama',
+        'value': 'AL'
+      },
+      {
+        'label': 'Alaska',
+        'value': 'AK'
+      },
+      {
+        'label': 'American Samoa',
+        'value': 'AS'
+      },
+      {
+        'label': 'Arizona',
+        'value': 'AZ'
+      },
+      {
+        'label': 'Arkansas',
+        'value': 'AR'
+      },
+      {
+        'label': 'California',
+        'value': 'CA'
+      },
+      {
+        'label': 'Colorado',
+        'value': 'CO'
+      },
+      {
+        'label': 'Connecticut',
+        'value': 'CT'
+      },
+      {
+        'label': 'Delaware',
+        'value': 'DE'
+      },
+      {
+        'label': 'District Of Columbia',
+        'value': 'DC'
+      },
+      {
+        'label': 'Federated States Of Micronesia',
+        'value': 'FM'
+      },
+      {
+        'label': 'Florida',
+        'value': 'FL'
+      },
+      {
+        'label': 'Georgia',
+        'value': 'GA'
+      },
+      {
+        'label': 'Guam',
+        'value': 'GU'
+      },
+      {
+        'label': 'Hawaii',
+        'value': 'HI'
+      },
+      {
+        'label': 'Idaho',
+        'value': 'ID'
+      },
+      {
+        'label': 'Illinois',
+        'value': 'IL'
+      },
+      {
+        'label': 'Indiana',
+        'value': 'IN'
+      },
+      {
+        'label': 'Iowa',
+        'value': 'IA'
+      },
+      {
+        'label': 'Kansas',
+        'value': 'KS'
+      },
+      {
+        'label': 'Kentucky',
+        'value': 'KY'
+      },
+      {
+        'label': 'Louisiana',
+        'value': 'LA'
+      },
+      {
+        'label': 'Maine',
+        'value': 'ME'
+      },
+      {
+        'label': 'Marshall Islands',
+        'value': 'MH'
+      },
+      {
+        'label': 'Maryland',
+        'value': 'MD'
+      },
+      {
+        'label': 'Massachusetts',
+        'value': 'MA'
+      },
+      {
+        'label': 'Michigan',
+        'value': 'MI'
+      },
+      {
+        'label': 'Minnesota',
+        'value': 'MN'
+      },
+      {
+        'label': 'Mississippi',
+        'value': 'MS'
+      },
+      {
+        'label': 'Missouri',
+        'value': 'MO'
+      },
+      {
+        'label': 'Montana',
+        'value': 'MT'
+      },
+      {
+        'label': 'Nebraska',
+        'value': 'NE'
+      },
+      {
+        'label': 'Nevada',
+        'value': 'NV'
+      },
+      {
+        'label': 'New Hampshire',
+        'value': 'NH'
+      },
+      {
+        'label': 'New Jersey',
+        'value': 'NJ'
+      },
+      {
+        'label': 'New Mexico',
+        'value': 'NM'
+      },
+      {
+        'label': 'New York',
+        'value': 'NY'
+      },
+      {
+        'label': 'North Carolina',
+        'value': 'NC'
+      },
+      {
+        'label': 'North Dakota',
+        'value': 'ND'
+      },
+      {
+        'label': 'Northern Mariana Islands',
+        'value': 'MP'
+      },
+      {
+        'label': 'Ohio',
+        'value': 'OH'
+      },
+      {
+        'label': 'Oklahoma',
+        'value': 'OK'
+      },
+      {
+        'label': 'Oregon',
+        'value': 'OR'
+      },
+      {
+        'label': 'Palau',
+        'value': 'PW'
+      },
+      {
+        'label': 'Pennsylvania',
+        'value': 'PA'
+      },
+      {
+        'label': 'Puerto Rico',
+        'value': 'PR'
+      },
+      {
+        'label': 'Rhode Island',
+        'value': 'RI'
+      },
+      {
+        'label': 'South Carolina',
+        'value': 'SC'
+      },
+      {
+        'label': 'South Dakota',
+        'value': 'SD'
+      },
+      {
+        'label': 'Tennessee',
+        'value': 'TN'
+      },
+      {
+        'label': 'Texas',
+        'value': 'TX'
+      },
+      {
+        'label': 'Utah',
+        'value': 'UT'
+      },
+      {
+        'label': 'Vermont',
+        'value': 'VT'
+      },
+      {
+        'label': 'Virgin Islands',
+        'value': 'VI'
+      },
+      {
+        'label': 'Virginia',
+        'value': 'VA'
+      },
+      {
+        'label': 'Washington',
+        'value': 'WA'
+      },
+      {
+        'label': 'West Virginia',
+        'value': 'WV'
+      },
+      {
+        'label': 'Wisconsin',
+        'value': 'WI'
+      },
+      {
+        'label': 'Wyoming',
+        'value': 'WY'
       }
+    ];
+    var results = [];
+    if (text.length === 0) {
+      results = states.splice(0);
+    } else {
+      results = states.filter(function(state) {
+        return (~state.label.toUpperCase().indexOf(text)) || (~state.value.toUpperCase().indexOf(text));
+      })
+    }
+
+    return results;
+  }
 });
--- autocomplete-selector.js
+++ autocomplete-selector.js
@@ -13,7 +13,7 @@
   @demo demo/index.html
 */
 Polymer({
-  is: "autocomplete-selector",
+  is: 'autocomplete-selector',
   hostAttributes: {
 
   },
@@ -27,13 +27,13 @@ Polymer({
     },
     /*
      * Unique property in the results object used to maintain the selected items
-     * 
+     *
      * This property is only used when the multi property is active
-     * 
+     *
      * For Example:
-     * 
+     *
      * In autocomplete result object { label : 'label', value: 'value', uid: 'uid'} to use uid this field should be 'uid'
-     * 
+     *
      */
     uidField: {
       type: String,
@@ -44,38 +44,38 @@ Polymer({
       type: String,
       value: 'Enter few characters'
     },
-    /* 
+    /*
      * Selected list of items only used if multi property is active
      */
     selectedList: {
       type: Array,
-      value: function () {
+      value: function() {
         return [];
       }
     },
     /*
       * Allows multiple selection in autocomplete, autocomplete doesnt close after the first selection
-      * 
+      *
       */
     multi: {
       type: Boolean,
       value: false
     },
     /*
-    * Fires search request on focus of the autocomplete text 
+    * Fires search request on focus of the autocomplete text
     */
     triggerOnFocus: {
       type: Boolean,
       value: false
     },
-    /* 
+    /*
      * Text to Show when no items are found for the input.
      */
     noItemsLabel: {
       type: String,
-      value: "No Items Found"
+      value: 'No Items Found'
     },
-    /* 
+    /*
     * Autocomplete results. should be used to bind with autcomplete-selectors data.
     * @type { Array => Object } Object Must have keys label and value
     */
@@ -84,23 +84,23 @@ Polymer({
       value: [],
       observer: 'show'
     },
-    /* 
-     * Selected value 
+    /*
+     * Selected value
      */
     value: {
-      type: Object,
+      type: Object
     },
-    /* 
+    /*
     *  Search querytext that is currently active
     */
     searchValue: {
       type: String,
-      observer: "_valueChanged",
+      observer: '_valueChanged',
       notify: true,
       value: null,
       reflectToAttribute: true
     },
-    /* 
+    /*
     *  The current value highlighted using mouse or keyboard, this is not the selected value of autocomplete. For selected value use value;
     */
     currentSelection: {
@@ -134,7 +134,7 @@ Polymer({
     'blur': '_clearAndClose'
   },
 
-  _focussed: function () {
+  _focussed: function() {
     if (!this.showingContainer && this.triggerOnFocus) {
       this.set('progress', true);
       this.set('showingContainer', true);
@@ -144,17 +144,17 @@ Polymer({
     }
   },
 
-  ready: function () {
+  ready: function() {
     this.$.resultList.render()
   },
 
-  _showEmpty: function () {
-    return this.data.length == 0;
+  _showEmpty: function() {
+    return this.data.length === 0;
   },
 
-  attached: function () {
-    var handler = function (event) {
-      if (event && (event.path.indexOf(this) != -1)) {
+  attached: function() {
+    var handler = function(event) {
+      if (event && (event.path.indexOf(this) !== -1)) {
         return;
       }
       this._clearAndClose();
@@ -163,64 +163,64 @@ Polymer({
     document.addEventListener('click', this.exitHandler);
   },
 
-  detached: function () {
+  detached: function() {
     document.removeEventListener('click', this.exitHandler);
   },
 
-
-  _highlightSelection: function (newValue, oldValue) {
+  _highlightSelection: function(newValue, oldValue) {
     var newLiId = '#li' + newValue;
     var newLi = this.$$(newLiId);
     if (oldValue || oldValue === 0) {
       var oldLiId = '#li' + oldValue;
       var oldLi = this.$$(oldLiId);
-      if (this.data[oldValue])
+      if (this.data[oldValue]) {
         this.data[oldValue].selected = false;
+      }
     }
     if (this.data[newValue]) {
       this.data[newValue].selected = true;
     }
     this.currentSelection = this.data[newValue];
     if (newLi) {
-      newLi.classList.add("selected");
-      newLi.classList.remove("unselected");
+      newLi.classList.add('selected');
+      newLi.classList.remove('unselected');
     }
     if (oldLi) {
-      oldLi.classList.remove("selected");
-      oldLi.classList.add("unselected");
+      oldLi.classList.remove('selected');
+      oldLi.classList.add('unselected');
     }
   },
 
-  _rippleId: function (index) {
+  _rippleId: function(index) {
     return 'ripple' + index;
   },
 
-  _liId: function (index) {
+  _liId: function(index) {
     return 'li' + index;
   },
 
-  _getClass: function (index) {
-    if (this.selectedIndex == index)
-      return "selected";
-    else
-      return "unselected";
+  _getClass: function(index) {
+    if (this.selectedIndex === index) {
+      return 'selected';
+    } else {
+      return 'unselected';
+    }
   },
 
-  _mouseover: function (event) {
+  _mouseover: function(event) {
     this.set('selectedIndex', event.model.index);
   },
 
-  _keyHandler: function (event) {
-    if (event.detail.keyboardEvent.keyCode == 27) {
+  _keyHandler: function(event) {
+    if (event.detail.keyboardEvent.keyCode === 27) {
       this._clearAndClose();
-    }
-    else if (event.detail.keyboardEvent.keyCode == 38) {
-      if ((this.selectedIndex) == 0) {
+    } else if (event.detail.keyboardEvent.keyCode === 38) {
+      if ((this.selectedIndex) === 0) {
         return;
       }
       this.selectedIndex--;
-    } else if (event.detail.keyboardEvent.keyCode == 40) {
-      if (this.selectedIndex == (this.data.length - 1)) {
+    } else if (event.detail.keyboardEvent.keyCode === 40) {
+      if (this.selectedIndex === (this.data.length - 1)) {
         return;
       }
       this.selectedIndex++;
@@ -230,15 +230,15 @@ Polymer({
     this._updateScroll();
   },
 
-  _updateScroll: function () {
-    var ul = this.$$("#ul");
-    var li = this.$$("#li" + this.selectedIndex);
+  _updateScroll: function() {
+    var ul = this.$$('#ul');
+    var li = this.$$('#li' + this.selectedIndex);
     if (!li) {
       return;
     }
     var top = li.offsetTop,
-      bot = top + li.offsetHeight,
-      hgt = ul.clientHeight;
+        bot = top + li.offsetHeight,
+        hgt = ul.clientHeight;
     if (top < ul.scrollTop) {
       ul.scrollTop = top;
     } else if (bot > ul.scrollTop + hgt) {
@@ -246,12 +246,12 @@ Polymer({
     }
   },
 
-  /* 
+  /*
    * Shows the autocomplete with the data provided
-   * 
+   *
    * @param {Array<Object>} List of items to be shown in the autocomplete selector
    */
-  show: function (data) {
+  show: function(data) {
     this.set('progress', false);
     var collapse = this.$.collapse
     this.selectedIndex = 0;
@@ -260,17 +260,17 @@ Polymer({
       this.data[this.selectedIndex].selected = true;
       this.currentSelection = this.data[this.selectedIndex];
     }
-    if (!collapse.opened && this.searchValue != "") {
+    if (!collapse.opened && this.searchValue !== '') {
       collapse.toggle()
       this.$.resultList.render();
     }
   },
 
-  _valueChanged: function (text) {
-    if (text === null) return;
+  _valueChanged: function(text) {
+    if (text === null) { return; }
     var collapse = this.$.collapse
-    if (text != '' || this.triggerOnFocus) {
-      this.debounce('search', function () {
+    if (text !== '' || this.triggerOnFocus) {
+      this.debounce('search', function() {
         this.set('progress', true);
         this.set('showingContainer', true);
         /**
@@ -286,18 +286,21 @@ Polymer({
     }
   },
 
-  _listFilter: function (item) {
+  _listFilter: function(item) {
     return item.name.toLowerCase().includes(
       this.searchValue.toLowerCase()
     )
   },
 
-  _clearAndClose: function (event) {
+  _clearAndClose: function(event) {
+    let triggerOnFocus = this.triggerOnFocus;
+    this.triggerOnFocus = false;
+    this.set('searchValue', '');
     this.set('showingContainer', false);
+    this.triggerOnFocus = triggerOnFocus;
   },
 
-
-  _selectCurrentItem: function (event) {
+  _selectCurrentItem: function(event) {
     var item = {};
     var index = 0;
     if (event) {
@@ -306,7 +309,7 @@ Polymer({
     } else {
       item = this.currentSelection;
       index = this.selectedIndex;
-      this.$$("#ripple" + this.selectedIndex).simulatedRipple();
+      this.$$('#ripple' + this.selectedIndex).simulatedRipple();
     }
     this.set('value', item);
     /**
@@ -325,7 +328,7 @@ Polymer({
     this.selectedList.push(item);
     let set = new Set();
     var self = this;
-    let unique = this.selectedList.filter(function (currentItem) {
+    let unique = this.selectedList.filter(function(currentItem) {
       var isUnique = set.has(currentItem[self.uidField])
       set.add(currentItem[self.uidField])
       return !isUnique;
@@ -333,7 +336,7 @@ Polymer({
     this.set('selectedList', unique);
   },
 
-  _removeSelection: function (event) {
+  _removeSelection: function(event) {
     this.splice('selectedList', event.model.index, 1);
   }
 });

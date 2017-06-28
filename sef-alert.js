ebc39037bae11f0726ffdee27c7f53287cfb986c
Lisa Catalano <@lcatdesigns (twitter)>
2017-06-28 14:11:29 -0500

    #1059 - Fix SEF alert code (Copied wrong version)

--- sef-alert.js
+++ sef-alert.js
@@ -1,7 +1,9 @@
 (function () {
   'use strict';
+
   Polymer({
     is: 'sef-alert',
+
     properties: {
       type: {
         type: String,
@@ -13,6 +15,14 @@
         notify: true,
         reflectToAttribute: true
       },
+      dismissable: {
+        type: String,
+        value: true
+      },
+      dismissClass: {
+        type: String,
+        computed: 'alertClass(dismissable)'
+      },
       duration: {
         type: Number,
         value: -1
@@ -22,40 +32,65 @@
         value: '',
         notify: true,
         reflectToAttribute: true
+      },
+      typeClass: {
+        type: String,
+        computed: 'convertClass(type)'
       }
     },
 
-    showSuccess: function (message, duration) {
-      duration = duration || this.duration;
-      this.set('type', 'Success')
-      this.set('message', message);
-      this.$.alert.classList.add('alert', 'alert-success');
-      this.set('show', true);
+    attached: function () {
       if (this.duration > 0) {
         this.async(function () {
           this.set('show', false);
-          this.$.alert.classList.remove('alert', 'alert-success');
-        }, duration);
+        }, this.duration);
       }
+    },
+
+    alertClass(dismiss) {
+      return (dismiss) ? 'alert-dismissable' : '';
+    },
+
+    // need lowercase classname for the css, but Proper Case for message
+    convertClass: function (type) {
+      return type.toLowerCase();
     },
 
     closeAlert: function () {
       this.set('show', false);
     },
 
-    showDanger: function (message, duration) {
+    showMessage: function (message, type, duration) {
       duration = duration || this.duration;
-      this.set('type', 'Error');
+
       this.set('message', message);
-      this.$.alert.classList.add('alert', 'alert-danger');
+      this.set('type', type);
       this.set('show', true);
-      //Do not hide if error occurs
-      //if (this.duration > 0) {
-      //  this.async(function() {
-      //    this.set('show', false);
-      //    this.$.alert.classList.remove('alert', 'alert-danger');
-      //  }, duration);
-      //}
+
+      if (duration > 0) {
+        this.async(function () {
+          this.set('show', false);
+        }, duration);
+      }
+    },
+
+    //
+    // Public methods to show Alert messages, depending on type
+    // --------------------------------------------------------
+    showError: function (message, duration = -1) {
+      this.showMessage(message, 'Error', duration);
+    },
+
+    showInfo: function (message, duration = 10000) {
+      this.showMessage(message, 'Info', duration);
+    },
+
+    showSuccess: function (message, duration = 5000) {
+      this.showMessage(message, 'Success', duration);
+    },
+
+    showWarning: function (message, duration = 10000) {
+      this.showMessage(message, 'Warning', duration);
     }
   });
 })();
